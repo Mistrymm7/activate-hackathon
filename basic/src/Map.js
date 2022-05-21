9ca420ca3bba943679a8f9fd604a96384f4f21a8
unknown <Administrator@CHILT7092.perkinswill.net>
2022-05-21 16:26:28 -0400

    Second

--- basic/src/Map.js
+++ basic/src/Map.js
@@ -8,9 +8,9 @@ mapboxgl.accessToken =
 const Map = () => {
   const mapContainerRef = useRef(null);
 
-  const [lng, setLng] = useState(5);
-  const [lat, setLat] = useState(34);
-  const [zoom, setZoom] = useState(1.5);
+  const [lng, setLng] = useState(31.1656);
+  const [lat, setLat] = useState(48.3794);
+  const [zoom, setZoom] = useState(7);
 
   // Initialize map when component mounts
   useEffect(() => {
